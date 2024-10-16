a338cef730308d7a0e504ae8b099bbaf2689f564
franco22bellido <franco.bellido22@gmail.com>
2024-10-16 00:08:53 -0300

    se crea formulario para setear el tiempo total del reloj

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { useState, useEffect} from "react";
 // Seria mejor que los timers para cada jugador sean un array en lugar de un estado cada uno.
 // un formulario para elegir el tiempo de la partida.
 // cuanto es el turno de alguien, que se deshabilite el boton
+// cuando un contador llegue a cero, entonces habra ganado el equipo contrario
 function App() {
   const [timeLeft, setTimeLeft] = useState(0);
   const [timePlayerOne, setTimePlayerOne] = useState(0);
   const [timePlayerTwo, setTimePlayerTwo] = useState(0);
   const [turn, setTurn] = useState(0);
   const [timerId, setTimerId] = useState(0);
+  const [inputValue, setInputValue] = useState('');
+
 
   const setTimeOfPlayers = ()=> {
       setTimePlayerOne(timeLeft / 2);
@@ -25,9 +28,9 @@ function App() {
   }
 
   const decrementTimeLeft = () => {
-    if( turn === 1 ) {
+    if( turn === 1 && timePlayerOne !== 0 ) {
       setTimePlayerOne(timePlayerOne => timePlayerOne - 1)
-    } else if( turn === 2) {
+    } else if( turn === 2 && timePlayerTwo !== 0) {
       setTimePlayerTwo(timePlayerTwo => timePlayerTwo - 1)
     }
   }
@@ -38,7 +41,7 @@ function App() {
 
   
   useEffect(()=> {
-    startTimeLeft();
+    if(turn !== 0) startTimeLeft();
   },[turn])
   useEffect(()=> {
     setTimeOfPlayers();
@@ -46,7 +49,10 @@ function App() {
   return (
     <div className="flex-center">
       {/* <h1>Chess clock</h1> */}
-      <button onClick={() => { setTimeLeft(10*60) }}>setear tiempo</button>
+      <form className="form" onSubmit={(e)=> {e.preventDefault(); setTurn(0);setTimeLeft(inputValue*60); setInputValue('')}}>
+        <input className="form-input" type="number" placeholder="Escribe el tiempo total" value={inputValue} onChange={(e)=> {setInputValue(e.target.value)}}/>
+        <button type="submit">Establecer tiempo</button>
+      </form>
       <button disabled={turn === 1 ? true : false} className="player-button" onClick={()=> changeTurn(1)}> {Math.floor(timePlayerOne / 60)}:{timePlayerOne % 60}</button>
       <button disabled={turn === 2 ? true : false} className="player-button" onClick={()=> changeTurn(2)}>{Math.floor(timePlayerTwo / 60)}:{timePlayerTwo % 60}</button>
     </div>
